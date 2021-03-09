bf401db647f3b13b6c09843ec80feb0a86d01b8c
killer5600 <52920192+killer5600@users.noreply.github.com>
2021-03-09 06:16:45 -0500

    z

---
+++ cc9/ez.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>| not found :(</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	</head>
+	<body style="background-color: #000">
+		<section class="text-center" style="padding-top: 200px">
+			<img src="https://i.pinimg.com/564x/bc/31/4f/bc314f95055834748b04322d8048ec4f.jpg">
+			<div style="color: #EFE7E7; font-size: 30px;">
+				s c r i p t e r
+			</div>
+			<div style="color:#787878; font-size: 15px;">
+				this site was out of service 
+			</div>
+			 <a href="https://discord.gg/qmz8prARm5">
+			 	<img src="https://maxcdn.icons8.com/Share/icon/Logos/discord_logo1600.png" style="height: 40px; width: 40px;">
+			 </a>
+		</section>
+	</body>
+</html>
